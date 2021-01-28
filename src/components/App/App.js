858236cad8da9772a08abeb4a198e20c421e81bc
summerlightningman <div8@bk.ru>
2021-01-28 18:48:06 +0300

    added providers into App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import Header from "../header/header";
@@ -5,26 +6,36 @@ import AddProduct from "../add-product/add-product";
 import Products from "../products/products";
 import Cart from "../cart/cart";
 
+import {CartProvider} from "../../contexts/CartContext";
+import {ProductsProvider} from "../../contexts/ProductsContext";
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 const App = () => {
-  return (
-      <Router>
-        <div className="container">
-            <Header/>
-            <Route exact path="/">
-                <Products/>
-            </Route>
-            <Route path="/add-product">
-                <AddProduct/>
-            </Route>
-            <Route path="/cart">
-                <Cart/>
-            </Route>
-        </div>
-      </Router>
-  );
+    const [cart, setCart] = useState([]);
+    const [products, setProducts] = useState([]);
+
+    return (
+        <Router>
+            <CartProvider value={cart}>
+                <ProductsProvider value={products}>
+                    <div className="container">
+                        <Header/>
+                        <Route exact path="/">
+                            <Products/>
+                        </Route>
+                        <Route path="/add-product">
+                            <AddProduct/>
+                        </Route>
+                        <Route path="/cart">
+                            <Cart/>
+                        </Route>
+                    </div>
+                </ProductsProvider>
+            </CartProvider>
+        </Router>
+    );
 }
 
 export default App;
