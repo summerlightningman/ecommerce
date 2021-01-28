9d4a55c94c26da76cbe9b4637c084a6696fbf96d
summerlightningman <div8@bk.ru>
2021-01-28 18:38:32 +0300

    going to Context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,29 @@
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+import Header from "../header/header";
+import AddProduct from "../add-product/add-product";
+import Products from "../products/products";
+import Cart from "../cart/cart";
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 const App = () => {
   return (
-      <h1>Component</h1>
+      <Router>
+        <div className="container">
+            <Header/>
+            <Route exact path="/">
+                <Products/>
+            </Route>
+            <Route path="/add-product">
+                <AddProduct/>
+            </Route>
+            <Route path="/cart">
+                <Cart/>
+            </Route>
+        </div>
+      </Router>
   );
 }
 
